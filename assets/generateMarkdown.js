0047e8e6606a135a6d4348dfbb1587dc295f2e7d
Ben Merchant <benmerch@gmail.com>
2024-07-24 21:34:07 -0500

    created the questions and put the answers in the README

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -12,9 +12,132 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contribute}
+
+## Tests
+
+${data.test}
+
+# Questions
+
+Check out my github repos at https://github.com/${data.github}
+
+If you have questions or wish to contact me, please email me at https://github.com/${data.email}
 
 `;
 }
+
+
+
+// const fs = require("fs");
+
+// function generateReadMe(){
+//   const file = `
+  
+//   #Create a Node.js App
+
+//   Work with a partner
+
+//   * As a developer, blah blah bla
+
+//   * As a dev, do the other thing too
+
+//   `
+
+//   fs.writeFile("README.md", file, function(err){
+//     if(err) console.log(err)
+//   } )
+// }
+
+// generateReadMe()
+
+
+
+/*
+
+function generateHtml(responseObj){
+    const html = `
+
+    <html>
+        <head>
+
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>Portfolio</title>
+
+        </head>
+
+        <body>
+            <div class="container">
+                <div class="row justify-content-centerl justify-text-center">
+                    <div class = "d-flex col justify-content-center">
+                        <h1>My Name is ${responseObj.name}</h1>
+                    </div>
+                <div class="container">
+                    <div class = "d-flex row">
+                        <div class = "col-sm align-content-center justify-content-center">
+                            <p>My job title is ${responseObj.title}</p>
+                        </div>
+                        <div class = "col-sm align-content-center justify-content-center">
+                            <p>I live in ${responseObj.location}</p>
+                        </div>
+                        <div class = "col-sm align-content-center justify-content-center">
+                            <p>${responseObj.job}</p>
+                        </div>
+                            <p>${responseObj.bio}</p>
+                        <div class = "col-sm align-content-center justify-content-center">
+                            Click my <a href = "${responseObj.linkedin}" target="_blank">LinkedIn Profile</a> to see more.
+                        </div>
+                        <div class = "col-sm align-content-center justify-content-center">
+                            Click my <a href = "${responseObj.github}" target="_blank">Github Profile</a> to see more of my code.
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </body>
+    
+    </html>
+    `
+
+    return html
+}
+
+
+module.exports = {
+    generateHtml
+}
+
+
+*/
+
+
+
+
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [function getUserQuestions(){
+    return [
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "What is the description of the project?",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "What is your github username?",
+        name: "github"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "Tell people how they can contribute to the project.",
+        name: "contribute"
+      },
+      {
+        type: "input",
+        message: "Tell people how to test the project.",
+        name: "test"
+      },
+      {
+        type: "input",
+        message: "What is your GitHub URL profile?",
+        name: "github"
+      },
+    ]
+  }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
